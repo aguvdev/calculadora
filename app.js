7aad52a97e40a55ed1b3506af74647f838dbd64c
hannagustinvega <hannheitiragustin@hotmail.com>
2021-05-12 15:49:02 -0300

    agregue el switch

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const {sumar, restar, multiplicar, dividir} = require ('./caculadora');
+
+switch (process.argv[2]) {
+    case "sumar":
+        sumar(process.argv[3])
+        break;
+
+    default:
+        break;
+}
